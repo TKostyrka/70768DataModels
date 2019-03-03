8fbc5d8bc8a5e17bbfb21bcffaf55ee99a9a73e1
Tomasz Kostyrka <TKostyrkaWSEI@users.noreply.github.com>
2019-03-03 09:19:09 +0100

    dim product

--- _dodatkowe/DimProd.sql
+++ _dodatkowe/DimProd.sql
@@ -1,31 +1,33 @@
-USE [ContosoRetailDW]
+use contosoretaildw
 GO
 
-CREATE VIEW [olap].[vDimProduct]
+ALTER VIEW [olap].[vDimProduct]
 AS
-SELECT	p.[ProductKey]
-      ,	p.[ProductLabel]
-      ,	p.[ProductName]
-      ,	p.[ProductDescription]
-      ,	p.[Manufacturer]
-      ,	p.[BrandName]
-      ,	p.[ClassID]
-      ,	p.[ClassName]
-      ,	p.[StyleID]
-      ,	p.[StyleName]
-      ,	p.[ColorID]
-      ,	p.[ColorName]
-
-      ,	s.[ProductSubcategoryLabel]
-      ,	s.[ProductSubcategoryName]
-      ,	s.[ProductSubcategoryDescription]
-
-      ,	c.[ProductCategoryLabel]
-      ,	c.[ProductCategoryName]
-      ,	c.[ProductCategoryDescription]
-FROM	
+SELECT
+	p.[ProductKey] + 0 AS [ProductKey]
+,	p.[ProductLabel]
+,	p.[ProductName]
+,	p.[ClassID]
+,	p.[ClassName]
+,	p.[StockTypeID]
+,	p.[StockTypeName]
+,	p.[ColorID]
+,	p.[ColorName]
+,	p.[UnitOfMeasureID]
+,	p.[UnitOfMeasureName]
+,	s.[ProductSubcategoryKey]		AS [SubcategoryKey]		--	<<	z subcategory
+,	s.[ProductSubcategoryLabel]		AS [SubcategoryLabel]
+,	s.[ProductSubcategoryName]		AS [SubcategoryName]	--	<<	z subcategory
+,	c.[ProductCategoryKey]			AS [CategoryKey]			--	<< z category
+,	c.[ProductCategoryLabel]		AS [CategoryLabel]
+,	c.[ProductCategoryName]			AS [CategoryName]			--	<< z category
+FROM 
 			[dbo].[DimProduct]				AS p
-INNER JOIN	[dbo].[DimProductSubcategory]	AS s	
-	ON p.ProductSubcategoryKey = s.ProductSubcategoryKey
-INNER JOIN	[dbo].[DimProductCategory]		AS c	
-	ON s.ProductCategoryKey = c.ProductCategoryKey
+INNER JOIN	[dbo].[DimProductSubcategory]	AS s ON p.[ProductSubcategoryKey]	= s.[ProductSubcategoryKey]
+INNER JOIN	[dbo].[DimProductCategory]		AS c ON s.[ProductCategoryKey]		= c.[ProductCategoryKey]
+GO
+
+--	1. dodaæ do DSV, ustawiæ klucz
+--	2. dodaæ wymiar (ustawiæ nazwê od razu), w atrybutach dodaæ wszystkie ID oraz Klucze
+--	3. dodaæ hierarchiê kategorii + atribute relationship (na kluczach)
+--	4. na atrybutach kluczy oraz ID podmieniæ wyœwietlane nazwy
